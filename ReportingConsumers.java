692e2a02f77fb45d3ce1e3a4ae7b7e4d2cf1a62e
Simon Lavers <simonlavers3@outlook.com>
2025-12-26 19:12:53 -0400

    Full report

--- ReportingConsumers.java
+++ ReportingConsumers.java
@@ -1,14 +1,44 @@
 package FunctionalProgrammingProject;
+import java.util.ArrayList;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
+import java.util.List;
 public class ReportingConsumers implements GradeApplications {
 
 
     static Consumer<Student> printStudentReport = student -> System.out.println("Student: " + student.getName()
-                    + " \nEmail: " + student.getEmail()
-                    + "  \nAverage: " + calculateAverage.apply(student)
+            + " \nEmail: " + student.getEmail()
+            + "  \nAverage: " + calculateAverage.apply(student)
     );
 
+    //Logs failing students
+    static Consumer<Student> logFailingStudent = student -> {
 
+        student.getGrades().forEach(grades -> {
+
+            if (getLetterGrade.apply(grades).equals("f"))
+
+                System.out.println("Failed Student");
+
+
+        });
+
+        System.out.println("Passing!");
+    };
+
+    static Consumer<Student> fullStudentReport = student -> printStudentReport.andThen(logFailingStudent);
+
+
+    //Outputs a warning if students average is lower than threshold
+    static BiConsumer<Student, Double> warnBeloThreshold = (student, threshold) -> {
+        double avg = calculateAverage.apply(student);
+
+        if(avg>=threshold) {System.out.println("Student is above threshold");}
+
+        else{System.out.println("WARNING: Student is below threshold");}
+
+
+    };
 
 }
